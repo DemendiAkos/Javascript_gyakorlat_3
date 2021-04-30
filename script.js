b5b4af547f3dda2b14259e6efe8873a0f13fb368
DemendiAkos <akosdemendi@gmail.com>
2021-04-30 14:46:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let body = document.querySelector(".list");
+let number = -1;
+
+
+
+for ( n = 0 ; n <= 20; n++ ) {
+    let newRow = document.createElement("tr");
+    let newNumber = document.createElement("td");
+    number++;
+    let newItem = document.createElement("td");
+    body.appendChild (newRow);
+    newNumber.textContent = number + "!";
+    newRow.appendChild (newNumber);
+    newItem.textContent = fac(n);
+    newRow.appendChild (newItem);
+    
+    function fac(n){
+        
+        return(n<2)?1:fac(n-1)*n;
+    }
+}
